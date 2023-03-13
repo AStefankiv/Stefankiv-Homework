c6ded1653f0db59db3177d145dda3549d3be2c10
AStefankiv <127073085+AStefankiv@users.noreply.github.com>
2023-03-12 23:27:03 -0600

    My homework 3, dated March 12, 2023

---
+++ HW3 (directory)/task_1_data_type.py
@@ -0,0 +1,27 @@
+print("I love Python" * 42)
+years = 31
+one_year = 12
+age_in_months = years * one_year
+print(age_in_months)
+age_in_years = years
+print(years)
+name = "Andrii"
+my_age = f"My name is {name}. I am {years} old."
+print(my_age)
+banana = 1
+print(banana == 1)
+banana != 2
+print(banana != 2)
+banana < 9
+print(banana < 9)
+banana > 0.5
+print(banana > 0.5)
+banana >= 3
+print(banana >= 3)
+banana <= 1
+print(banana <= 1)
+a = 2
+b = 5
+c = 6
+d = f"{a}{b}{c}"
+print(d)
