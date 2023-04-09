d6617e2419b66923f14f60f57d5419d5e5efd592
AStefankiv <127073085+AStefankiv@users.noreply.github.com>
2023-04-09 17:48:14 -0600

    Delete 10_recursion - part 1.py
    Signed-off-by: AStefankiv <127073085+AStefankiv@users.noreply.github.com>

--- HW_10/10_recursion - part 1.py
+++
@@ -1,9 +0,0 @@
-# HW 1
-def recursion(n: int) -> None:
-    print(n, end=" ")
-    if n == 0:
-        return None
-    return recursion(n - 1)
-
-enter = int(input("Enter any number: "))
-result = recursion(enter)
