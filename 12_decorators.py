e6b606c72494432eccb8c7d5e33809de87b4185d
AStefankiv <127073085+AStefankiv@users.noreply.github.com>
2023-04-27 21:33:15 -0600

    Homework 12 - 27th of April - Stefankiv Andrii

--- 12_decorators.py
+++ 12_decorators.py
@@ -2,53 +2,71 @@
 
 import time
 
-def decorator_time_name(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.strftime('%d-%m-%Y %H:%M:%S')
-        result = func(*args, **kwargs)
-        print(f"Function {func.__name__} was launched at {start_time}")
-        return result
-    return wrapper
-
-@decorator_time_name
-def simple_function():
-    print("What's the time and function's name?")
-
-simple_function()
-
-@decorator_time_name
-def multiply_add_function(a, b, c):
-    return a * b + c
-
-print(multiply_add_function(3, 4, 5))
+# def decorator_time_name(func):
+#     def wrapper(*args, **kwargs):
+#         start_time = time.strftime('%d-%m-%Y %H:%M:%S')
+#         result = func(*args, **kwargs)
+#         print(f"Function {func.__name__} was launched at {start_time}")
+#         return result
+#     return wrapper
+#
+# @decorator_time_name
+# def simple_function():
+#     print("What's the time and function's name?")
+#
+# simple_function()
+#
+# @decorator_time_name
+# def multiply_add_function(a, b, c):
+#     return a * b + c
+#
+# print(multiply_add_function(3, 4, 5))
 
 # HW - 2
 
-class MyManager:
-
-    print("==========")
-    def __enter__(self):
-        print("Enter method")
-        return 1
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Exit method")
-
-    print("==========")
+# class MyManager:
+#     def __enter__(self):
+#         print("==========")
+#
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         print("==========")
+#         if exc_type == KeyError:
+#             print(f"Key {exc_val} does not exist")
+#
+#
+# with MyManager():
+#     print("Something inside my context manager")
 
 # HW - 3
 
-def decorator_time_name(func):
-    def wrapper(times):
-        start_time = time.strftime('%d-%m-%Y %H:%M:%S')
-        result = func(times)
-        for i in range(times):
-            print(f"Function {func.__name__} was launched at {start_time}")
-        return result
-    return wrapper
+# def decorator_time_name(func):
+#     def wrapper(times):
+#         start_time = time.strftime('%d-%m-%Y %H:%M:%S')
+#         for i in range(times):
+#             print(f"Function {func.__name__} was launched at {start_time}")
+#         return func()
+#     return wrapper
+#
+# @decorator_time_name(times=3)
+# def simple_function():
+#     print("What's the time and function's name?")
+#
+# simple_function()
 
-@decorator_time_name
-def simple_function(*args):
-    print("What's the time and function's name?")
 
-simple_function(6)
+def decorator_time_name(times):
+    def wrapper_1(func):
+        def wrapper_2(*args, **kwargs):
+            for i in range(times):
+                start_time = time.strftime('%d-%m-%Y %H:%M:%S')
+                result = func(*args, **kwargs)
+                print(f"Function {func.__name__} was launched at {start_time}")
+            return result
+        return wrapper_2
+    return wrapper_1
+
+@decorator_time_name(times=3)
+def simple_function():
+    print("What's the function's name and the time?")
+
+simple_function()
